88a1dd79b77cdc61a4f094717444eb30ff0fff63
dhruv bhuva <dhruvbhuva@dhruvs-MacBook-Air-3.local>
2026-08-16 17:08:54 +0530

    Add password strength validation

--- login.cpp
+++ login.cpp
@@ -11,3 +11,19 @@ bool authenticate(const User& user, const string& username,
                   const string& password) {
     return user.username == username && user.password == password;
 }
+
+bool isValidPassword(const string& password) {
+    if (password.length() < 8) {
+        return false;
+    }
+
+    bool hasDigit = false;
+    for (char ch : password) {
+        if (ch >= '0' && ch <= '9') {
+            hasDigit = true;
+            break;
+        }
+    }
+
+    return hasDigit;
+}
